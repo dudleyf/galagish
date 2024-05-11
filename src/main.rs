ed1461a3f00daf71e8ca850082b17dc7ae1fc5be
Dudley Flanders <dudley.flanders@vendr.com>
2024-05-11 18:24:52 -0500

    Projectiles and collisions

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use bevy::input::keyboard::KeyboardInput;
+use bevy::math::bounding::{Aabb2d, IntersectsVolume};
 use bevy::prelude::*;
 use bevy::sprite::MaterialMesh2dBundle;
+
 
 #[derive(Component)]
 struct Player;
@@ -14,6 +16,11 @@ struct Projectile;
 #[derive(Component, Deref, DerefMut)]
 struct Velocity(Vec2);
 
+#[derive(Resource)]
+struct ProjectileTimer(Timer);
+
+#[derive(Component)]
+struct Enemy;
 
 const PADDLE_SIZE: Vec3 = Vec3::new(120.0, 20.0, 0.0);
 const PADDLE_COLOR: Color = Color::rgb(0.3, 0.3, 0.7);
@@ -24,8 +31,18 @@ const PROJECTILE_COLOR: Color = Color::rgb(0.95, 0.95, 0.95);
 const INITIAL_PROJECTILE_DIRECTION: Vec2 = Vec2::new(0.5, 0.5);
 const PROJECTILE_SPEED: f32 = 400.0;
 const PLAYER_STARTING_POSITION: Vec3 = Vec3::new(0.0, -300.0, 1.0);
+const TOP_OF_SCREEN: f32 = 350.0;
+const PROJECTILE_COOLDOWN_SECONDS: f32 = 0.3;
+const PROJECTILE_STARTING_POSITION: Vec3 = Vec3::new(0.0, 20.0, 1.0);
 
-fn setup_game(mut commands: Commands) {
+fn setup_game(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    asset_server: Res<AssetServer>,
+) {
+    commands.spawn(Camera2dBundle::default());
+
     commands.spawn((
         SpriteBundle {
             transform: Transform {
@@ -42,7 +59,18 @@ fn setup_game(mut commands: Commands) {
         Player,
         Collider,
     ));
-    commands.spawn(Camera2dBundle::default());
+
+    commands.spawn((
+        MaterialMesh2dBundle {
+            mesh: meshes.add(Circle::default()).into(),
+            material: materials.add(ColorMaterial::from(PROJECTILE_COLOR)),
+            transform: Transform::from_translation(PROJECTILE_STARTING_POSITION)
+                .with_scale(PROJECTILE_SIZE * Vec3::new(2.0, 2.0, 2.0)),
+            ..default()
+        },
+        Enemy,
+        Collider,
+    ));
 }
 
 
@@ -65,6 +93,8 @@ fn move_player(keyboard_input: Res<ButtonInput<KeyCode>>, mut query: Query<&mut 
 
 
 fn shoot_projectile(
+    time: Res<Time>,
+    mut projectile_timer: ResMut<ProjectileTimer>,
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
@@ -73,17 +103,21 @@ fn shoot_projectile(
 ) {
     let player_transform = query.single_mut();
     if keyboard_input.pressed(KeyCode::Space) {
-        commands.spawn((
-            MaterialMesh2dBundle {
-                mesh: meshes.add(Circle::default()).into(),
-                material: materials.add(ColorMaterial::from(PROJECTILE_COLOR)),
-                transform: Transform::from_translation(player_transform.translation)
-                    .with_scale(PROJECTILE_SIZE),
-                ..default()
-            },
-            Projectile,
-            Velocity(INITIAL_PROJECTILE_DIRECTION.normalize() * PROJECTILE_SPEED),
+        if projectile_timer.0.tick(time.delta()).finished() {
+            projectile_timer.0.reset();
+
+            commands.spawn((
+                MaterialMesh2dBundle {
+                    mesh: meshes.add(Circle::default()).into(),
+                    material: materials.add(ColorMaterial::from(PROJECTILE_COLOR)),
+                    transform: Transform::from_translation(player_transform.translation)
+                        .with_scale(PROJECTILE_SIZE),
+                    ..default()
+                },
+                Projectile,
+                Velocity(INITIAL_PROJECTILE_DIRECTION.normalize() * PROJECTILE_SPEED),
             ));
+        }
     }
 }
 
@@ -94,10 +128,59 @@ fn move_projectiles(mut query: Query<&mut Transform, With<Projectile>>) {
     }
 }
 
+fn destroy_projectiles(
+    mut commands: Commands,
+    query: Query<(Entity, &Transform), With<Projectile>>,
+) {
+    for (collider_entity, collider_transform) in &query {
+        if collider_transform.translation.y > TOP_OF_SCREEN {
+            commands.entity(collider_entity).despawn();
+        }
+    }
+}
+
+fn check_for_collisions(
+    mut commands: Commands,
+    projectiles_query: Query<(Entity, &Transform), With<Projectile>>,
+    collider_query: Query<(Entity, &Transform, Option<&Enemy>), With<Collider>>,
+) {
+    for (projectile_entity, projectile_transform) in &projectiles_query {
+        for (collider_entity, collider_transform, enemy_check) in &collider_query {
+            let projectile_box = Aabb2d::new(
+                projectile_transform.translation.truncate(),
+                projectile_transform.scale.truncate() / 2.0
+            );
+            let collider_box = Aabb2d::new(
+                collider_transform.translation.truncate(),
+                collider_transform.scale.truncate() / 2.0,
+            );
+            let collision = projectile_box.intersects(&collider_box);
+            if collision {
+                if enemy_check.is_some() {
+                    println!("Collided!");
+                    // Enemy is destroyed
+                    commands.entity(collider_entity).despawn();
+                    // Projectile disappears too? Prevents "cutting through" a line of enemies all at once
+                    commands.entity(projectile_entity).despawn();
+                }
+            }
+        }
+    }
+}
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .insert_resource(ProjectileTimer(Timer::from_seconds(PROJECTILE_COOLDOWN_SECONDS, TimerMode::Once)))
         .add_systems(Startup, setup_game)
-        .add_systems(Update, (move_player, shoot_projectile, move_projectiles))
+        .add_systems(Update, check_for_collisions)
+        .add_systems(FixedUpdate, (
+            move_player,
+            shoot_projectile,
+            move_projectiles,
+            destroy_projectiles
+        ).before(check_for_collisions))
+        .insert_resource(Time::<Fixed>::from_seconds(TIME_STEP as f64))
+        .add_systems(Update, bevy::window::close_on_esc)
         .run();
 }
